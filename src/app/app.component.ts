3d13ab19df1649df4c5837d8f33fa990ba7c096d
HuaTsai <huatsai.eed07g@nctu.edu.tw>
2024-01-28 04:37:17 +0800

    feat: intro, fontawesome, animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,23 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SongListComponent } from './song-list/song-list.component';
 import { ContainerComponent } from './container/container.component';
+import { FontAwesomeModule, FaIconLibrary, FaConfig } from '@fortawesome/angular-fontawesome';
+import { faYoutube, faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SongListComponent, ContainerComponent],
+  imports: [CommonModule, RouterOutlet, SongListComponent, ContainerComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'sotisleepy';
+  constructor(faConfig: FaConfig, library: FaIconLibrary) {
+    faConfig.defaultPrefix = 'fab';
+    faConfig.fixedWidth = true;
+    library.addIcons(faYoutube);
+    library.addIcons(faFacebook);
+    library.addIcons(faTwitter);
+    library.addIcons(faInstagram);
+  }
 }
--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { FaIconComponent, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-intro',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, FontAwesomeModule],
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.scss'
 })
 export class IntroComponent {
+  animatedIcon: string | null = null;
 
+  onMouseEnter(icon: string) {
+    this.animatedIcon = icon;
+  }
+
+  onMouseLeave() {
+    this.animatedIcon = null;
+  }
 }
