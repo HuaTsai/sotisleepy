92509bbb900f676d45e8d5242e81f9851484ff6c
HuaTsai <huatsai.eed07g@nctu.edu.tw>
2024-02-17 05:18:37 +0800

    fix: cors issue (long time figure out)

--- src/app/services/song.service.ts
+++ src/app/services/song.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class SongService {
   // TODO: use http stream & share replay, make apiUrl configurable
-  // readonly apiUrl = 'http://localhost:4200';
+  // readonly apiUrl = 'http://localhost:8000';
   readonly apiUrl = 'https://sotis-website-412614.de.r.appspot.com';
 
   streamSongList: StreamSong[] = [];
@@ -17,6 +17,6 @@ export class SongService {
   constructor(private http: HttpClient) {}
 
   getCovers(): Observable<CoverSong[]> {
-    return this.http.get<CoverSong[]>(this.apiUrl + '/covers');
+    return this.http.get<CoverSong[]>(this.apiUrl + '/covers/');
   }
 }
